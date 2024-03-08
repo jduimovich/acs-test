aee07eb1999932ae803983c126fe67c0de99d243
John Duimovich <jduimovich@gmail.com>
2024-03-08 15:39:13 -0500

    promote

--- promote-to-prod.sh
+++ promote-to-prod.sh
@@ -1,6 +1,7 @@
 
 
 
-git checkout promote-to-prod
+git checkout -b promote-to-prod
+git checkout  promote-to-prod
 git commit --allow-empty -m "Trigger promotion to stage $D" 
-git push
+ git push --set-upstream origin promote-to-prod
--- promote-to-stage.sh
+++ promote-to-stage.sh
@@ -1,4 +1,5 @@
-
-git checkout promote-to-stage
+ 
+git checkout -b promote-to-stage
+git checkout  promote-to-stage
 git commit --allow-empty -m "Trigger promotion to stage $D" 
-git push
+ git push --set-upstream origin promote-to-stage
